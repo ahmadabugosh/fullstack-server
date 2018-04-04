dcf25e9f34e708c89e6c42bf73f8822d0d696dde
Ahmad Abugosh <ahmadabugosh@gmail.com>
2018-04-05 00:09:23 +0400

    Enabled cookies

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require ('express');
 const mongoose =require('mongoose');
 require('./models/User');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./services/passport'); 
 const keys =require('./config/keys');
 
-const app =express();
+
 
 mongoose.connect(keys.mongoURI);
+const app =express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
