7f4ee60d1f8147562a137fabc186ccdfeebb2cf5
Ahmad Abugosh <ahmadabugosh@gmail.com>
2018-04-10 15:24:47 +0400

    Heroku proxy production for Google oauth

--- services/passport.js
+++ services/passport.js
@@ -16,21 +16,22 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-passport.use(new GoogleStrategy({
-
-	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret,
-	callbackURL: '/auth/google/callback'},
-	proxy:true,
-
-(accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleID: profile.id }).then(existingUser => {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           // we already have a record with the given profile ID
           done(null, existingUser);
         } else {
           // we don't have a user record with this ID, make a new record!
-          new User({ googleID: profile.id })
+          new User({ googleId: profile.id })
             .save()
             .then(user => done(null, user));
         }
