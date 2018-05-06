0d0e64351e9671fdc5291cb498819f3b9698e9f8
Ahmad Abugosh <ahmadabugosh@gmail.com>
2018-05-06 14:03:40 +0400

    Added survey model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const mongoose =require('mongoose');
 require('./models/User');
+require('./models/Survey');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 require('./services/passport');
